dbfb6cede5281f66585e1314e9ea84dac3a09240
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-07 21:16:00 +0100

    add task01 solution

---
+++ src/homework25/task02/Main.java
@@ -0,0 +1,22 @@
+package homework25.task02;
+/**
+ * 07/11/2023 homework  * @author Boris Iurciuc (cohort36)
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Pen myPen = new Pen();
+    myPen.inkLevel = 1000;
+    myPen.inkForChar = 10;
+    myPen.text = "Привет, меня зовут Джон, мне 36 !";
+
+    myPen.checkInk();              // метод - проверка достаточности чернил для данного текста
+
+    if (myPen.checkInkLevel <= myPen.inkLevel ) {       // условие достаточности чернил
+      myPen.write();              // метод - вывод текста на консоль
+      myPen.refill();             // метод - восстановление уровня чернил
+    } else {
+      System.out.println("текст слишком длинный");
+    }
+  }
+}
