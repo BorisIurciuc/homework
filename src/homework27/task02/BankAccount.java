0d3e76a48ac49698bd07802e44e5db8e3a84cd6b
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-12 11:58:03 +0100

    edit task02 hw27

--- src/homework27/task02/BankAccount.java
+++ src/homework27/task02/BankAccount.java
@@ -7,7 +7,6 @@ public class BankAccount {
 
   private final long accountNumber;     // Номер счета
   private int moneyAmount = 10;         // Текущая сумма на счете
-  private int cashFlows;                // Сумма для перечисления
 
   BankAccount(long accountNumber) {
     this.accountNumber = accountNumber;
@@ -21,32 +20,22 @@ public class BankAccount {
     return moneyAmount;
   }
 
-  // Сумма для перечисления
-  public int getCashFlows() {                   // получить сумму перечисления
-    return cashFlows;
-  }
-
-  public void setCashFlows(int cashFlows) {     // установить сумму перечисления
-    this.cashFlows = cashFlows;
-  }
-
   //Операции снять и положить на счет
-  public void pushMoneyAmount() {                         // Перечислить на счет
-    this.moneyAmount = getMoneyAmount() + getCashFlows();
+  public void pushMoneyAmount(int cash) {                         // Перечислить на счет
+    this.moneyAmount = getMoneyAmount() + cash;
   }
 
-  public void pullMoneyAmount() {                         // Снять со счёта
-    if (0 < getMoneyAmount() - getCashFlows()) {
-      this.moneyAmount = getMoneyAmount() - getCashFlows();
+  public void pullMoneyAmount(int cash) {                         // Снять со счёта
+    if (0 < getMoneyAmount() - cash) {
+      this.moneyAmount = getMoneyAmount() - cash;
     } else {
       System.out.println("Недостаточно денежных средств");
-      return;
     }
   }
 
   // Вывод данных на консоль
   public void print() {
-    System.out.println("Account number  : " + getAccountNumber() +
-        "\nAmount of money : " + getMoneyAmount());
+    System.out.println(
+        "Account number  : " + getAccountNumber() + "\nAmount of money : " + getMoneyAmount());
   }
 }
--- src/homework27/task02/Main.java
+++ src/homework27/task02/Main.java
@@ -1,7 +1,6 @@
 package homework27.task02;
 
 import java.util.Scanner;
-
 /**
  * 11/11/2023 homework  * @author Boris Iurciuc (cohort36)
  */
@@ -9,21 +8,21 @@ public class Main {
 
   public static void main(String[] args) {
 
-  BankAccount bankAccount1 = new BankAccount(100010001); // Объект счет в банке
+  BankAccount bankAccount1 = new BankAccount(10010001); // Объект счет в банке
   bankAccount1.print();
 
   Scanner scanner = new Scanner(System.in);
   System.out.println("\nВыберете операцию: положить на счет < 1 >  или снять с счета < 2 >");
   int typeOperation = scanner.nextInt();
+
   System.out.print("введите сумму для операции : ");
   int cash = scanner.nextInt();       // Сумма для перечисления
-  bankAccount1.setCashFlows(cash);
 
   if (typeOperation == 1) {
-    bankAccount1.pushMoneyAmount();   // Вызов метода перечислить на счет
+    bankAccount1.pushMoneyAmount(cash);   // Вызов метода перечислить на счет
   }
   if (typeOperation == 2) {
-    bankAccount1.pullMoneyAmount();   // Вызов метода снять со счёта
+    bankAccount1.pullMoneyAmount(cash);   // Вызов метода снять со счёта
   }
   bankAccount1.print();
   }
