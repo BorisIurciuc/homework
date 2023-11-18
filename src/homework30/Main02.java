48f3a8a7bb79b576eadc2d5d4c70b650238109ef
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-18 14:41:01 +0100

    add hw30

---
+++ src/homework30/Main02.java
@@ -0,0 +1,19 @@
+package homework30;
+
+/**
+ * 18/11/2023 homework * @author Boris Iurciuc (cohort36)
+ */
+public class Main02 {
+
+  public static void main(String[] args) {
+
+    int a = 5;
+    int b = 6;
+    int c = 4;
+    double d = 8;
+
+    double rez = a * (b - d) + c * a / d;
+
+    System.out.println(rez);
+  }
+}
