4bf5e22c89c75fe8f75bb4182e0e3b9f46a9693d
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-26 18:23:18 +0100

    edit hw33 Main03

--- src/homework33/Main03.java
+++ src/homework33/Main03.java
@@ -8,14 +8,15 @@ import java.util.Scanner;
 public class Main03 {
   // Метод, который сгенерирует первые n чисел последовательности Фибоначчи.
   // n передать как параметр в метод.
-  public static void fibonacci(int n) {
-    int[] f = new int[n];
+  public static void fibonacci(int n, int[] f) {
     f[0] = 0;
     f[1] = 1;
     for (int i = 2; i < n; i++) {
       f[i] = f[i - 1] + f[i - 2];
     }
-    for (int i = 0; i < n; i++) {
+  }
+  public static void print(int[] f) {
+    for (int i = 0; i < f.length; i++) {
       System.out.print(f[i] + " ");
     }
   }
@@ -24,6 +25,8 @@ public class Main03 {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Enter n : ");
     int n = scanner.nextInt();
-    fibonacci(n);
+    int[] f = new int[n];
+    fibonacci(n,f);
+    print(f);
   }
 }
