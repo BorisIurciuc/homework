1322d1198e880fdd0d160a8c328c3f2222f6bd2b
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-11-26 19:10:14 +0100

    edit hw33 Main02

--- src/homework33/Main02.java
+++ src/homework33/Main02.java
@@ -1,20 +1,22 @@
 package homework33;
-
 /**
  * 25/11/2023 homework * @author Boris Iurciuc (cohort36)
  */
 public class Main02 {
 
   //метод, возвращающий массив типа char, в котором записан английский алфавит (маленькими буквами)
-  public static void alphabet(char[] ch) {
+  public static char[] alphabet(char[] ch) {
       for (int i = 97; i < 123; i++) {
       ch[i] = (char) i;
-      System.out.print(ch[i] + " ");
-    }
+      }
+      return ch;
   }
-
   public static void main(String[] args) {
     char[] ch = new char[123];
     alphabet(ch);
+    for (int i = 97; i < 123; i++) {
+      System.out.print(ch[i] + " ");
+    }
   }
 }
+g
