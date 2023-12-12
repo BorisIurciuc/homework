ff8228647d7c765a3930976a88dbc7aa089b7498
Boris Iurciuc <boris.iurciuc@gmail.com>
2023-12-12 10:46:30 +0100

    edit hw39 task02 + addition task

--- src/homework39/task02/Main.java
+++ src/homework39/task02/Main.java
@@ -4,24 +4,44 @@ package homework39.task02;
  * 11/12/2023 myCode * @author Boris Iurciuc (cohort36)
  */
 public class Main {
-
-    public static void getStarted(int numberOfStaff, Employee[] employee) {
-       for (int i = 0; i < numberOfStaff; i++) {
-        System.out.print(employee[i]);
-        employee[i].work();
+  public static void promotion(Employee[] employee){
+    String[] name = {"Bill", "John"};
+    for (int i = 0; i < employee.length; i++) {
+      if (employee[i] instanceof Manager)  {
+        employee[i].setSalary(7500 + 1000);
       }
+      if (employee[1] instanceof Developer) {
+        employee[1] = new Manager(name[1], 7500 );
+      }
+      break;
     }
-
-  public static void main(String[] args) {
-    Employee[] employee = new Employee[10];
+  }
+  public static void getStarted(Employee[] employee) {
+    for (int i = 0; i < employee.length; i++) {
+      System.out.print(employee[i]);
+      employee[i].work();
+    }
+  }
+  public static void setEmployee(Employee[] employee){
     String[] name = {"Bill", "John", "Jek", "Bob", "Kate", "Mary", "Sam", "Peter", "Helen","Tom"};
     for (int i = 0; i < employee.length; i++) {
       if (i < 1) {
-        employee[i] = new Manager(name[i], 7500 );
+        employee[i] = new Manager(name[i], 7500);
       } else {
         employee[i] = new Developer(name[i], 5000 + i * 10 );
       }
     }
-    getStarted(10, employee);
+  }
+
+  public static void main(String[] args) {
+
+    Employee[] employee = new Employee[10];
+
+    setEmployee(employee);
+    getStarted(employee);
+    System.out.println("\nPromotion ");
+    promotion(employee);
+    getStarted(employee);
   }
 }
+
