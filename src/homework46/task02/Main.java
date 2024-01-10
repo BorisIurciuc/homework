a76902287c02c0bdae68450315fe180a85f9f288
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-01-10 09:39:29 +0100

    add hw46 task02

---
+++ src/homework46/task02/Main.java
@@ -0,0 +1,50 @@
+package homework46.task02;
+
+import java.util.Scanner;
+
+/**
+ * 09/01/2024 homework * @author Boris Iurciuc (cohort36)
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int day;
+    while (true) {
+      System.out.print("Enter integer number 1 - 7 : ");
+       day = scanner.nextInt();
+      if (day > 0 && day < 8) {
+        break;
+      } else  {
+        System.out.println("Error, number ");
+      }
+    }
+
+    switch (day) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+      default:
+        System.out.println("Check the number");
+    }
+  }
+}
