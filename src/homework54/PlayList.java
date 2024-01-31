8cefbb2ba3c8916b1a206fe08aec5e808845cf83
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-01-31 16:58:59 +0100

    hw54: edit methods, add method to main

--- src/homework54/PlayList.java
+++ src/homework54/PlayList.java
@@ -3,7 +3,6 @@ package homework54;
 import static java.util.Collections.shuffle;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
@@ -20,58 +19,44 @@ public class PlayList {
     songList.add(song);
   }
 
-  public void addSongByIndex(int index, Song song){
+  public void addSongByIndex(int index, Song song) {
     System.out.print("After add by index ");
-    songList.add(index,song);
+    songList.add(index, song);
   }
-
+  public int getSize(){
+    return songList.size();
+  }
   public Song getSongIndex(int index) {
-
     return songList.get(index);
   }
 
   public void removeByIndex(int index) {
-    System.out.print("After removeByIndex ");
     songList.remove(index);
   }
 
   public void removeByTitle(String title) {
     System.out.print("After remove ByTitle ");
-    for (int i = 0; i < songList.size(); i++ ) {
-      songList.removeIf(song -> song.getTitle().equals(title));;
+    for (int i = 0; i < songList.size(); i++) {
+      songList.removeIf(song -> song.getTitle().equals(title));
     }
   }
 
-  public void getListeningQueue() {
-    System.out.println("\ngetListeningQueue");
-    Iterator<Song> iteratorS = songList.iterator();
-    while (iteratorS.hasNext()) {
-      System.out.println(iteratorS.next());
-    }
+  public List<Song> getListeningQueue() {
+    return songList;
   }
 
-  public void getShuffledListeningQueue() {
-    System.out.println("\ngetShuffledListeningQueue");
+  public List<Song> getShuffledListeningQueue() {
     List<Song> shuffledList = new LinkedList<>(songList);
     shuffle(shuffledList);
-    for (Song song : shuffledList) {
-      System.out.println(song);
-    }
+    return shuffledList;
   }
 
-  public void getReverseListeningQueue() {
-    System.out.println("\ngetReverseListeningQueue");
+  public List<Song> getReverseListeningQueue() {
+    List<Song> reversedList = new ArrayList<>(songList.size());
     ListIterator<Song> iteratorS = songList.listIterator(songList.size());
     while (iteratorS.hasPrevious()) {
-      System.out.println(iteratorS.previous());
+      reversedList.add(iteratorS.previous());
     }
-  }
-
-  public void listenThePlaylist(){
-    Iterator<Song> iteratorS = songList.iterator();
-    System.out.println("послушать плейлист ");
-    while (iteratorS.hasNext()) {
-      System.out.println("Играет песня:" + iteratorS.next());
-    }
+    return reversedList;
   }
 }
